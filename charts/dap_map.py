72cf0850984a25b7ef80958ab846249afe340f67
Jstavik <80905915+Jstavik@users.noreply.github.com>
2026-06-02 11:36:10 +0000

    test: hardcode red textfont in dap_map

--- charts/dap_map.py
+++ charts/dap_map.py
@@ -35,7 +35,7 @@ def fig_dap_map(df: pd.DataFrame) -> go.Figure:
                 f"{cc} {r['base']:.0f}|{r['peak']:.0f}€\n"
                 f"Δ{r['dod_base']:+.0f}|Δ{r['dod_peak']:+.0f}€"
             ],
-            textfont=dict(size=11, color=color, family="Arial Black"),
+            textfont=dict(size=12, color="#FF0000"),
             hovertemplate=(
                 f"<b>{cc}</b><br>"
                 f"Base: <b>{r['base']:.1f} EUR/MWh</b><br>"
